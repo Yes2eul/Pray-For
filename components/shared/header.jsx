15f02d31323cd4e6f082f359ad64ec06eb982876
Yes2eul <yeseul5611@gmail.com>
2024-05-09 16:34:28 +0900

    feat: firebase 로그아웃

--- components/shared/header.jsx
+++ components/shared/header.jsx
@@ -1,10 +1,20 @@
 "use client";
 
+import { auth } from "@/utils/firebase";
 import { useRouter } from "next/navigation";
 import React from "react";
 
 const Header = () => {
   const router = useRouter();
+
+  const handleLogout = async () => {
+    try {
+      await auth.signOut();
+      router.push("/");
+    } catch (error) {
+      console.error(error.message);
+    }
+  };
   return (
     <header id="header">
       <div onClick={() => router.push("/")}>
@@ -13,8 +23,8 @@ const Header = () => {
       <nav>
         <ul>
           <li onClick={() => router.push("/main")}>HOME</li>
-          <li onClick={() => router.push("/")}>MYPAGE</li>
-          <li onClick={() => router.push("/")}>LOGOUT</li>
+          <li>MYPAGE</li>
+          <li onClick={handleLogout}>LOGOUT</li>
         </ul>
       </nav>
     </header>
