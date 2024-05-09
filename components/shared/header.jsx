4e9907a60ffe945d2e13f3260e03735929cc8788
Yes2eul <yeseul5611@gmail.com>
2024-05-09 16:16:29 +0900

    feat: header nav 수정

--- components/shared/header.jsx
+++ components/shared/header.jsx
@@ -2,7 +2,6 @@
 
 import { useRouter } from "next/navigation";
 import React from "react";
-// import ChurchIcon from "@/public/church.svg";
 
 const Header = () => {
   const router = useRouter();
@@ -10,8 +9,14 @@ const Header = () => {
     <header id="header">
       <div onClick={() => router.push("/")}>
         <h1>Pray For</h1>
-        {/* <ChurchIcon width="33" height="33" /> */}
       </div>
+      <nav>
+        <ul>
+          <li onClick={() => router.push("/main")}>HOME</li>
+          <li onClick={() => router.push("/")}>MYPAGE</li>
+          <li onClick={() => router.push("/")}>LOGOUT</li>
+        </ul>
+      </nav>
     </header>
   );
 };
