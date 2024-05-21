c57eefe1b972f7e6b5dc1c79b7c97576e8bfde3f
Yes2eul <yeseul5611@gmail.com>
2024-05-21 17:20:51 +0900

    feat: 뒤로가기 추가

--- components/profile/userPost.js
+++ components/profile/userPost.js
@@ -3,13 +3,18 @@ import React from "react";
 import { useAuth } from "@/hooks/useAuth";
 import UseUserInfo from "@/hooks/useUserInfo";
 import getTimeDifference from "@/hooks/getTimeDifference";
+import { useRouter } from "next/navigation";
 
 const UserPost = () => {
   const { user } = useAuth();
   const userPosts = UseUserInfo(user);
+  const router = useRouter();
 
   return (
     <>
+      <button onClick={() => router.back()}>
+        <p>{"<"}</p>
+      </button>
       {userPosts && (
         <ul>
           {userPosts.posts.map((post, index) => (
